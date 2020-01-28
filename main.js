25bbc0f1e0eca1d7a162126bcdcce3cd3950c340
Kenny Mc <kenymc1@gmail.com>
2020-01-27 21:17:10 -0600

    added events

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const pies = [
     iceCream: 'nope',
     isAvailable: true,
     image:"img/pie_0005_Pizza.png",
-    drinkPairing: 'High Life',
+    drinkPairing: 'Red Wine',
     instructor: 'Luke',
   },
   {
@@ -45,7 +45,7 @@ const pies = [
     isAvailable: "Yes",
     image:"img/pie_0002_Chest.png",
     drinkPairing: 'High Life',
-    instructor: 'Mary',
+    instructor: 'Greg',
   },
   {
     name:"Chocolate",
@@ -81,7 +81,7 @@ const pies = [
     isAvailable: true,
     image:"img/pie_0004_blue-berry.png",
     drinkPairing: 'High Life',
-    instructor: 'Kenny',
+    instructor: 'Greg',
   }
 ]
 
@@ -91,22 +91,22 @@ const printToDom = (toPrint, divId) => {
   document.getElementById(divId).innerHTML = toPrint;
 };
 
-const pieBuilder = () => {
+const pieBuilder = (monkeybuttArray) => {
   let domString = '';
-  for (let i = 0; i < pies.length; i += 1) {
+  for (let i = 0; i < monkeybuttArray.length; i += 1) {
     domString += `
     <div class="pie-card">
-    <h1>${pies[i].name}</h1>
+    <h1>${monkeybuttArray[i].name}</h1>
     <hr>
-    <img src=${pies[i].image} alt="">
-    <h2>${pies[i].price}</h2>
-    <h2>Ice Cream: <br>${pies[i].iceCream}</h2>
+    <img src=${monkeybuttArray[i].image} alt="">
+    <h2>${monkeybuttArray[i].price}</h2>
+    <h2>Ice Cream: <br>${monkeybuttArray[i].iceCream}</h2>
     <hr>
-    <h3>Warm: <br>${pies[i].iswarm}</h3>
-    <h4>Crust: ${pies[i].crust}</h4>
-    <h5>Available: <br>${pies[i].isAvailable}</h5>
-    <h6>Drink Pairing: <br> <${pies[i].drinkPairing}</h6>
-    <h4 class="instructor">${pies[i].instructor}</h4>
+    <h3>Warm: <br>${monkeybuttArray[i].iswarm}</h3>
+    <h4>Crust: ${monkeybuttArray[i].crust}</h4>
+    <h5>Available: <br>${monkeybuttArray[i].isAvailable}</h5>
+    <h6>Drink Pairing: ${monkeybuttArray[i].drinkPairing}</h6>
+    <h4 class="instructor">${monkeybuttArray[i].instructor}</h4>
    
  </div>
 </div>
@@ -120,4 +120,59 @@ const pieBuilder = () => {
 
 
 
-pieBuilder()
+
+
+// const zoesPies = () => {
+//   const myPies = []
+//   for(let i = 0; i < pies.length; i += 1){
+//     if(pies[i].instructor === 'Zoe'){
+//       myPies.push(pies[i]);
+//     }
+//   }
+//   pieBuilder(myPies);
+// }
+// const maryPies = () => {
+//   const herPies = []
+//   for(let i = 0; i < pies.length; i += 1){
+//     if(pies[i].instructor === 'Mary'){
+//       herPies.push(pies[i]);
+//     }
+//   }
+//   pieBuilder(herPies);
+// }
+// const gregPies = () => {
+//   const hisPies = []
+//   for(let i = 0; i < pies.length; i += 1){
+//     if(pies[i].instructor === 'Greg'){
+//       hisPies.push(pies[i]);
+//     }
+//   }
+//   pieBuilder(hisPies);
+// }
+// const lukePies = () => {
+//   const therePies = []
+//   for(let i = 0; i < pies.length; i += 1){
+//     if(pies[i].instructor === 'Luke'){
+//       therePies.push(pies[i]);
+//     }
+//   }
+//   pieBuilder(therePies);
+// }
+
+const findMyPies = (e) => {
+  const buttonId = e.target.id;
+  const myPies = []
+  for(let i = 0; i < pies.length; i += 1){
+    if(pies[i].instructor === buttonId){
+      myPies.push(pies[i]);
+    }
+  }
+  pieBuilder(myPies);
+}
+
+pieBuilder(pies);
+
+document.getElementById('Zoe').addEventListener('click',findMyPies)
+document.getElementById('Mary').addEventListener('click',findMyPies)
+document.getElementById('Greg').addEventListener('click',findMyPies)
+document.getElementById('Luke').addEventListener('click',findMyPies)
